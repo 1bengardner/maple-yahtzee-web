c2714495b033333f60b2dcece921fbdc6f657a2f
Ben Gardner <1bengardner@gmail.com>
2026-08-15 22:44:39 -0400

    Add share

--- modal.js
+++ modal.js
@@ -28,6 +28,53 @@ function createIcon(score) {
   }
 }
 
+function createShareEmoji(score) {
+  const emojis = [
+    [1576, "🪦"],
+    [1575, "☠"],
+    [1500, "💀"],
+    [1450, "🦴🦴🦴🦴🦴🦴🦴🦴🦴🦴"],
+    [1400, "🦴🦴🦴🦴🦴🦴🦴🦴🦴"],
+    [1350, "🦴🦴🦴🦴🦴🦴🦴🦴"],
+    [1300, "🦴🦴🦴🦴🦴🦴🦴"],
+    [1250, "🦴🦴🦴🦴🦴🦴"],
+    [1200, "🦴🦴🦴🦴🦴"],
+    [1150, "🦴🦴🦴🦴"],
+    [1100, "🦴🦴🦴"],
+    [1050, "🦴🦴"],
+    [1000, "🦴"],
+    [950, "💥💥💥💥💥💥💥💥💥💥"],
+    [900, "💥💥💥💥💥💥💥💥💥"],
+    [850, "💥💥💥💥💥💥💥💥"],
+    [800, "💥💥💥💥💥💥💥"],
+    [750, "💥💥💥💥💥💥"],
+    [700, "💥💥💥💥💥"],
+    [650, "💥💥💥💥"],
+    [600, "💥💥💥"],
+    [550, "💥💥"],
+    [500, "💥"],
+    [450, "📣"],
+    [400, "📢"],
+    [350, "🐌🌱"],
+    [300, "🐼"],
+    [250, "🫚"],
+    [200, "🐖"],
+    [150, "🍄"],
+    [100, "🐌"],
+    [0, ""],
+  ];
+  for (const [target, emoji] of emojis) {
+    if (score >= target) {
+      return emoji;
+    }
+  }
+}
+
+function createDescriptionString(score, yahtzeeCount) {
+  const yahtzeeString = yahtzeeCount > 0 ? ` and ${yahtzeeCount} ${yahtzeeCount == 1 ? "yahtzee" : "yahtzees"}` : "";
+  return `You finished the game with ${score} points${yahtzeeString}.`;
+}
+
 function createStatHtml(value, content) {
   return `
     <div class="stat">
@@ -37,9 +84,21 @@ function createStatHtml(value, content) {
   `;
 }
 
-function createDescriptionString(score, yahtzeeCount) {
-  const yahtzeeString = yahtzeeCount > 0 ? ` and ${yahtzeeCount} ${yahtzeeCount == 1 ? "yahtzee" : "yahtzees"}` : "";
-  return `You finished the game with ${score} points${yahtzeeString}.`;
+function createShareHtml() {
+  return `
+    <button id="share" class="primary">
+      Share
+    </button>
+  `;
+}
+
+function share(score, yahtzeeCount) {
+  const yahtzeeString = yahtzeeCount > 0 ? `, including ${yahtzeeCount == 1 ? "a" : yahtzeeCount} ${yahtzeeCount == 1 ? "yahtzee" : "yahtzees"}` : "";
+  const body = `${createShareEmoji(score)} I got ${score} POINTS in Maple Yahtzee${yahtzeeString}!\u00A0🍁\n\nhttps://1bengardner.github.io/maple-yahtzee-web/`.trim();
+  navigator.share({
+    title: "Maple Yahtzee!",
+    text: body,
+  });
 }
 
 export function gameOver(parent, playAgain, { score, yahtzeeCount, gotBonus }) {
@@ -65,9 +124,7 @@ export function gameOver(parent, playAgain, { score, yahtzeeCount, gotBonus }) {
       ${yahtzeeCount > 0 ? createStatHtml(yahtzeeCount, yahtzeeCount == 1 ? "Yahtzee" : "Yahtzees") : ""}
     </div>
 
-    <!-- TODO <button class="primary">
-      Share
-    </button> -->
+    ${navigator.canShare ? createShareHtml() : ""}
 
     <button id="play-again" class="secondary">
       Play Again
@@ -80,6 +137,9 @@ export function gameOver(parent, playAgain, { score, yahtzeeCount, gotBonus }) {
   document.getElementById("play-again").addEventListener("click", () => {
     playAgain();
     modal.remove();
+  });
+  document.getElementById("share")?.addEventListener("click", () => {
+    share(score, yahtzeeCount);
   });
   document.querySelector(".modal .close").addEventListener("click", () => {
     modal.remove();
